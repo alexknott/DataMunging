2c8b24afd24240986aa5bd1e7713fdc56fc7e562
Alex Knott <netshark@live.co.uk>
2013-06-18 21:41:06 +0100

    Updated tests to use test file from embedded resource

--- FootballData.Tests/TeamCollectionFactoryTests.cs
+++ FootballData.Tests/TeamCollectionFactoryTests.cs
@@ -1,4 +1,7 @@
-﻿using CommonLibrary;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using CommonLibrary;
 using Moq;
 using NUnit.Framework;
 
@@ -42,37 +45,13 @@ namespace FootballData.Tests
 
         public string[] DummyTestCollection()
         {
-            var lines = new string[28];
-            lines[0] = "Source <a";
-            lines[1] = "href=\"http://sunsite.tut.fi/rec/riku/soccer_data/tab/93_94/table.eng0.01_02.html\">sunsite.tut.fi/rec/riku/soccer_data/tab/93_94/table.eng0.01_02.html</a>";
-            lines[2] = "";
-            lines[3] = "<pre>";
-            lines[4] = "       Team            P     W    L   D    F      A     Pts";
-            lines[5] = "    1. Arsenal         38    26   9   3    79  -  36    87";
-            lines[6] = "    2. Liverpool       38    24   8   6    67  -  30    80";
-            lines[7] = "    3. Manchester_U    38    24   5   9    87  -  45    77";
-            lines[8] = "    4. Newcastle       38    21   8   9    74  -  52    71";
-            lines[9] = "    5. Leeds           38    18  12   8    53  -  37    66";
-            lines[10] = "    6. Chelsea         38    17  13   8    66  -  38    64";
-            lines[11] = "    7. West_Ham        38    15   8  15    48  -  57    53";
-            lines[12] = "    8. Aston_Villa     38    12  14  12    46  -  47    50";
-            lines[13] = "    9. Tottenham       38    14   8  16    49  -  53    50";
-            lines[14] = "   10. Blackburn       38    12  10  16    55  -  51    46";
-            lines[15] = "   11. Southampton     38    12   9  17    46  -  54    45";
-            lines[16] = "   12. Middlesbrough   38    12   9  17    35  -  47    45";
-            lines[17] = "   13. Fulham          38    10  14  14    36  -  44    44";
-            lines[18] = "   14. Charlton        38    10  14  14    38  -  49    44";
-            lines[19] = "   15. Everton         38    11  10  17    45  -  57    43";
-            lines[20] = "   16. Bolton          38     9  13  16    44  -  62    40";
-            lines[21] = "   17. Sunderland      38    10  10  18    29  -  51    40";
-            lines[22] = "   -------------------------------------------------------";
-            lines[23] = "   18. Ipswich         38     9   9  20    41  -  64    36";
-            lines[24] = "   19. Derby           38     8   6  24    33  -  63    30";
-            lines[25] = "   20. Leicester       38     5  13  20    30  -  64    28";
-            lines[26] = "</pre>";
-            lines[27] = "";
+            var testFile = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("FootballData.Tests.Resources.football.dat"));
+            var lines = new List<string>();
 
-            return lines;
+            while(!testFile.EndOfStream)
+                lines.Add(testFile.ReadLine());
+
+            return lines.ToArray();
         }
     }
 }
--- WeatherData.Tests/WeatherDataProviderTests.cs
+++ WeatherData.Tests/WeatherDataProviderTests.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using CommonLibrary;
 using Moq;
 using NUnit.Framework;
@@ -14,21 +16,12 @@ namespace WeatherData.Tests
         [SetUp]
         public void TestSetUp()
         {
+            var rawDays = new List<string>();
+            var streamReader = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("WeatherData.Tests.Resources.weather.dat"));
+            while (!streamReader.EndOfStream)
+                rawDays.Add(streamReader.ReadLine());
+
             _fileSystemWrapper = new Mock<FileSystemFacade>();   
-            IList<string> rawDays = new List<string>();
-            rawDays.Add("(Unofficial, Preliminary Data). Source: <a");
-            rawDays.Add("href=\"http://www.erh.noaa.gov/er/box/dailystns.html\">www.erh.noaa.gov/er/box/dailystns.html</a>");
-            rawDays.Add("");
-            rawDays.Add("<pre>");
-            rawDays.Add("MMU June 2002");
-            rawDays.Add("");
-            rawDays.Add("  Dy MxT   MnT   AvT   HDDay  AvDP 1HrP TPcpn WxType PDir AvSp Dir MxS SkyC MxR MnR AvSLP");
-            rawDays.Add("");
-            rawDays.Add("   1  88    59    74          53.8       0.00 F       280  9.6 270  17  1.6  93 23 1004.5");
-            rawDays.Add("   2  79    63    71          46.5       0.00         330  8.7 340  23  3.3  70 28 1004.5");
-            rawDays.Add("  mo  82.9  60.5  71.7    16  58.8       0.00              6.9          5.3");
-            rawDays.Add("</pre>");
-            rawDays.Add("");
             _fileSystemWrapper.Setup(m => m.ReadAllLines(It.IsAny<string>())).Returns(rawDays.ToArray());
         }
 
@@ -38,16 +31,13 @@ namespace WeatherData.Tests
             WeatherDataProvider weatherDataProvider = new WeatherDataProvider(_fileSystemWrapper.Object);
             var days = weatherDataProvider.GetDays("").ToArray();
 
-            Assert.AreEqual(2, days.Length);
+            Assert.AreEqual(29, days.Length);
             Assert.AreEqual(1, days[0].DayNumber);
             Assert.AreEqual(88, days[0].Mxt);
             Assert.AreEqual(59, days[0].Mnt);
             Assert.AreEqual(2, days[1].DayNumber);
             Assert.AreEqual(79, days[1].Mxt);
             Assert.AreEqual(63, days[1].Mnt);
-            
         }
-
-
     }
 }
